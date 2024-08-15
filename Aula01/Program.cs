60535dd95738d5384b18ec3410a2247185d9f507
Improvez <lucaszanotto@hotmail.com>
2024-08-14 22:03:51 -0300

    add: Adicionando o conteudo da aula

---
+++ Aula01/Program.cs
@@ -0,0 +1,22 @@
+﻿// Comentário de uma linha
+
+/*
+    Comentário de 
+    Múltiplas linhas
+*/
+
+// Declarando variável 
+
+int number;
+
+// Atribuindo valor
+
+number = 10;
+
+// Declarando constante 
+
+const int DAYS_IN_WEEK = 7;
+
+// Imprimindo String concatenada
+
+Console.WriteLine($"A semana tem {DAYS_IN_WEEK} dias.");
